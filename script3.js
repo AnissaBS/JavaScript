6c732d5165fb7f21c2ff4a0d81227c06851b5f0b
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-09-05 16:43:25 +0200

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,7 +1,27 @@
     // Algorithme nombre
     
+// var n;
+
+// n = prompt ("Veuillez entrer un nombre n =");
+
+//     for(i=0; i<=10; i++){                          // "i++"" veut dire i+1
+//     document.write(n*i + "<br>")
+//     }
+
+
+
+
+    // Algorithme nombre2
+
 var n;
-n = prompt ("Veuillez entrer un nombre n =");
-for(i=0; i<=10; i++){                          // "i++"" veut dire i+1
-    document.write(n*i + "<br>")
+var resultat;
+var compteur = 0;
+resultat=0;
+
+n = prompt ("Veuillez entrer un nombre entier n =");
+
+while(resultat < 1000){
+    resultat = n*compteur;
+    compteur = compteur +1;
+    document.write(resultat + "<br>");
 }
