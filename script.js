05d4a5b02b02c4c77dc7fccf594a931074d1b629
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-10-04 16:41:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-    // Algorithme Inconnu
+/* Quand je clique sur l'icône */
+let icons = document.getElementById('icons');
 
-var nombre_inc = 10;
-var nombre_entre;
+/*Au clic j'aimerais que tu me sortes et caches la clsse active */
+icons.addEventListener("click", function(){
+    nav.classList.toggle("active");
+})
 
-nombre_entre = prompt("Quel est le nombre inconnu ?")
+/* En cliquant sur un de ces boutons, le navbar se re-range */
+// Pour ce faire, on pointe tous nos liens //
+const links = document.querySelectorAll("nav li"); /* On place toutes nos balises enfants (li) dans la variable links */
 
-if(nombre_entre > nombre_inc){
-    alert("C'est trop grand");
-}
-
-if(nombre_entre < nombre_inc){
-    alert("C'est trop petit");
-}
-
-if(nombre_entre == nombre_inc){ // " == " : pour que ce ne soit pas une affectation ("=") mais plutôt pour vérifier si les deux sont égaux
-    alert("C'est top !");
-} 
-    
+links.forEach(link =>{
+    link.addEventListener("click", ()=>{    /* ()=> équivaut à function{}; */
+        /* Les trois <li>, je les appelle link qui est un paramètre, puis j'ajoute un évènement click sur chacun des li, vérifiable avec un console.log(links);  */
+        nav.classList.remove("active"); /*Enfin, j'enlève la classe li si jamais on clique */
+    })
+});
